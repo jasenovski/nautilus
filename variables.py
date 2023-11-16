9c5c5558b2089424867162756ff8bad617c9dd74
jasenovski <igormjsk@gmail.com>
2023-11-16 14:21:39 -0300

    aumentadas para 123 acoes curadas pelo hideki

--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
-BR_STOCKS = ['BOVA11', 'QUAL3', 'AMER3', 'TAEE11', 'EGIE3', 'MILS3', 'MGLU3', 'YDUQ3', 'VIVT3', 'GUAR3', 'B3SA3', 'BPAN4', 'CSNA3', 'GFSA3', 'GRND3', 'TIMS3', 
+BR_STOCKS_ = ['BOVA11', 'QUAL3', 'AMER3', 'TAEE11', 'EGIE3', 'MILS3', 'MGLU3', 'YDUQ3', 'VIVT3', 'GUAR3', 'B3SA3', 'BPAN4', 'CSNA3', 'GFSA3', 'GRND3', 'TIMS3', 
              'LREN3', 'SBSP3', 'ARZZ3', 'UGPA3', 'ALPA4', 'SLCE3', 'CIEL3', 'GOAU4', 'FESA4', 'DIRR3', 'ETER3', 'ENAT3', 'BBSE3', 'FLRY3', 'TOTS3', 'ENEV3', 
              'USIM5', 'BRPR3', 'BRFS3', 'CSMG3', 'RENT3', 'MULT3', 'BRKM5', 'RADL3', 'PETR3', 'CCRO3', 'BEEF3', 'COGN3', 'ABEV3', 'CPLE6', 'KLBN11', 'ECOR3', 
              'CMIG4', 'CVCB3', 'RAPT4', 'ODPV3', 'POMO4', 'ANIM3', 'AGRO3', 'ENGI11', 'AMAR3', 'ALUP11', 'CPLE3', 'LUPA3', 'ITUB3', 'KLBN4', 'LIGT3', 'KEPL3', 
@@ -9,6 +9,14 @@ US_STOCKS = ['^GSPC', 'AAPL', 'MSFT', 'AMZN', 'GOOG', 'GOOGL', 'META', 'TSLA', '
              'NKE', 'AVGO', 'MCD', 'COST', 'QCOM', 'NEE', 'TXN', 'DHR', 'MDT', 'HON', 'UNP', 'LIN', 'AMGN', 'PM', 'SBUX', 'LLY', 'UPS', 'LOW', 'AMT', 
              'IBM', 'CAT', 'GILD', 'MMM', 'MO', 'GE', 'CHTR', 'NOW', 'BLK', 'INTU', 'SPGI', 'ISRG', 'AMD', 'CVS', 'ZTS', 'PLD', 'AXP', 'TGT', 'FIS', 'APD', 
              'MDLZ', 'ADP', 'LMT', 'CME', 'DUK', 'CL', 'ICE', 'BDX', 'CB', 'SPG', 'CI', 'NSC', 'VRTX', 'CCI', 'RTX', 'TMUS', 'BKNG', 'DE', 'TJX', 'SYK', 'TFC']
+
+BR_STOCKS = ['ABEV3', 'AGRO3', 'ALPA3', 'ALSO3', 'ALUP11', 'ANIM3', 'ARZZ3', 'B3SA3', 'BAZA3', 'BBAS3', 'BBDC3', 'BBSE3', 'BEEF3', 'BEES3', 'BHIA3', 'BMEB3', 'BNBR3', 'BRAP3', 
+              'BRFS3', 'BRKM3', 'BRSR3', 'BSLI3', 'CASN3', 'CCRO3', 'CEEB3', 'CEED3', 'CGAS3', 'CIEL3', 'CLSC3', 'CMIG3', 'COCE3', 'COGN3', 'CPFE3', 'CPLE3', 'CSAN3', 'CSMG3', 
+              'CSNA3', 'CVCB3', 'CYRE3', 'DASA3', 'DIRR3', 'DXCO3', 'ECOR3', 'EGIE3', 'ELET3', 'EMBR3', 'ENAT3', 'ENEV3', 'ENGI11', 'ENGI3', 'EQPA3', 'EQTL3', 'EVEN3', 'EZTC3', 
+              'FLRY3', 'FRAS3', 'GGBR3', 'GOAU3','GRND3','GUAR3','HYPE3','ITSA3','ITUB3','JBSS3','JHSF3','KEPL3','KLBN11','KLBN3','LEVE3','LIGT3','LOGN3','LREN3', 'MDIA3',
+              'MGLU3', 'MILS3', 'MOAR3', 'MRFG3', 'MRVE3', 'MULT3', 'MYPK3', 'NTCO3', 'ODPV3', 'OFSA3', 'PETR3', 'PNVL3', 'POMO3', 'POSI3', 'PRIO3', 'PSSA3', 'RADL3', 'RAIL3', 
+              'RANI3', 'RAPT3', 'REDE3', 'RENT3', 'ROMI3', 'SANB11', 'SANB3', 'SBSP3', 'SCAR3', 'SIMH3', 'SLCE3', 'SMTO3', 'TAEE11', 'TASA3', 'TELB3', 'TEND3', 'TGMA3', 'TIMS3',
+              'TOTS3' 'TRPL3' 'TUPY3' 'UGPA3' 'UNIP3' 'USIM3' 'VALE3' 'VIIA3' 'VIVT3' 'VLID3' 'VULC3' 'WEGE3' 'WHRL3' 'YDUQ3',]
 
 FERIADOS_BR = [
     "2023-05-01",
