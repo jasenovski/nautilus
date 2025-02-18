9c53ad8b33a7ba0598089db8019ba1102532ccb6
jasenovski <igormjsk@gmail.com>
2025-02-17 22:27:55 -0300

    first commit

---
+++ cotacoes/cotacoes.py
@@ -0,0 +1,119 @@
+import yfinance as yf
+from tqdm import tqdm
+from datetime import datetime, timedelta
+import requests
+import pandas as pd
+
+def busca_cotacoes(simbolos: list, intervalo: str, 
+                   **kwargs) -> pd.DataFrame:
+
+    """
+    Função que busca as variações periódicas das ações
+
+    Args:
+    simbolos (list): Lista com os símbolos (tickers) das ações
+    cotacoes_anteriores (int): Quantidade de cotações anteriores a serem buscadas para as variações das ações
+    kwargs (dict): dicionário com as chaves 'cotacoes_anteriores' e 'cotacoes_segurar' OU 'data_inicio' e 'data_fim'
+
+    Returns:
+    variacoes (pd.DataFrame): DataFrame com as variações periódicas das ações
+    """
+
+    # data de hoje (formato datetime)
+    hoje_dtm: datetime = datetime.today()
+
+    cotacoes_anteriores = kwargs.get('cotacoes_anteriores', None)
+    cotacoes_segurar = kwargs.get('cotacoes_segurar', None)
+
+    if cotacoes_anteriores is not None and cotacoes_segurar is not None:
+        # data de início da busca (data de hoje menos a quantidade de cotações anteriores)
+        if intervalo == "d":
+            # se o intervalo for diário, subtrai a quantidade de dias
+            data_inicio: datetime = hoje_dtm - timedelta(days=cotacoes_anteriores)
+            data_fim: datetime = hoje_dtm + timedelta(days=cotacoes_segurar)
+        elif intervalo == "w":
+            # se o intervalo for semanal, subtrai a quantidade de semanas
+            data_inicio: datetime = hoje_dtm - timedelta(weeks=cotacoes_anteriores)
+            data_fim: datetime = hoje_dtm + timedelta(weeks=cotacoes_segurar)
+        
+        # converte a data de início para string (aaaa-mm-dd)
+        data_inicio: str = data_inicio.strftime('%Y-%m-%d')
+        data_fim: str = data_fim.strftime('%Y-%m-%d')
+    else:
+        data_inicio = kwargs.get('data_inicio', None)
+        data_fim = kwargs.get('data_fim', None)
+
+        if data_inicio is None or data_fim is None:
+            raise ValueError("É necessário fornecer os parametros 'cotacoes_anteriores' e 'cotacoes_segurar'.")
+
+    # busca as cotações das ações para o intervalo especificado
+    cotacoes: pd.DataFrame = yf.download(simbolos, start=data_inicio, end=data_fim)['Adj Close']
+
+    # api_key = "640b20547955f2.19099552"
+    # cotacoes = pd.DataFrame(data=[], columns=["date", "open", "high", "low", "close", "adjusted_close", "volume"])
+    # for simbolo in tqdm(simbolos):
+    #     url = f'https://eodhd.com/api/eod/{simbolo}?from={data_inicio}&to={data_fim}&period={intervalo}&api_token={api_key}&fmt=json'
+    #     data = requests.get(url).json()
+    #     df = pd.DataFrame(data)
+    #     df["nm_acao"] = simbolo
+
+    #     # print(df)
+
+    #     cotacoes = pd.concat([cotacoes, df])
+
+    # cotacoes = cotacoes.pivot(index="date", columns="nm_acao", values="adjusted_close")
+    # # cotacoes[(cotacoes.index >= data_fim) & (cotacoes.index <= data_inicio)]
+    # cotacoes.columns.name = "Ticker"
+    # cotacoes.index.name = "Date"
+    # cotacoes.index = pd.to_datetime(cotacoes.index)
+
+    return cotacoes
+
+def formata_cotacoes(cotacoes: pd.DataFrame, maiores_medias: int) -> pd.DataFrame:
+
+    """
+    Função que formata as cotações das ações para variações periódicas e filtra as ações com maiores médias de retorno
+
+    Args:
+    cotacoes (pd.DataFrame): DataFrame com as cotações das ações
+    intervalo (str): Intervalo de busca das variações periódicas das ações. 'd' para diário, 'w' para semanal
+    maiores_medias (int): Quantidade de ações com maiores médias de retorno a serem filtradas
+
+    Returns:
+    variacoes_intervaladas_filtradas (pd.DataFrame): DataFrame com as variações periódicas das ações filtradas
+    """
+
+    series_percentuais_na = cotacoes.isna().sum() / cotacoes.shape[0]
+    acoes_excluir = series_percentuais_na[series_percentuais_na > 0.1].index.to_list()
+
+    # elimina as colunas (ações) que possuem mais de 10% de valores nulos
+    cotacoes.drop(columns=acoes_excluir, inplace=True)
+
+    # preenche os valores nulos com o último valor válido
+    cotacoes.fillna(method="ffill", inplace=True)
+
+    # elimina as colunas (axis = 1: nome das ações) que possuem valores nulos para datas específicas dentro do intervalo de busca    
+    cotacoes.dropna(axis=0, inplace=True)
+
+    # filtra as variações periódicas das ações (a cada 5 dias ou todos os dias)
+    # cotacoes_intervaladas: pd.DataFrame = \
+    #     cotacoes.iloc[::5] if intervalo == "w" else cotacoes.iloc[::1]
+
+    # calcula as variações diárias das ações e elimina as linhas com valores nulos.
+    # valores nulos podem ocorrer quando a ação não possui cotação em um determinado dia
+    variacoes: pd.DataFrame = cotacoes.pct_change().dropna()    
+
+    if maiores_medias > 0:
+        # calcula as médias dos retornos das ações
+        medias: pd.Series = variacoes.mean(axis=0)
+
+        # o método 'nlargest' está presente em qualquer objeto do tipo 'Series'. 
+        # Esse método retorna outro 'Series' com os 'n' maiores valores
+        acoes_maiores_medias: pd.Series = medias.nlargest(maiores_medias)
+
+        # pega as ações com as maiores médias de retorno
+        variacoes_filtradas: pd.DataFrame = variacoes.loc[:, acoes_maiores_medias.index]
+
+        return variacoes_filtradas, acoes_excluir
+    
+    return variacoes, acoes_excluir
